d2a4a972b744158d445f7d62e793dd1f802889e7
29hannah <hannah.ziesenies@gmail.com>
2022-04-14 11:54:54 +0200

    list of values, mean, stdev, boxplot

--- centroid_analysis.py
+++ centroid_analysis.py
@@ -5,6 +5,7 @@ import pathlib
 from os.path import join
 import matplotlib.pyplot as plt
 
+
 def abs_file_paths(directory):
     for dirpath, _, filenames in os.walk(directory):
         for f in filenames:
@@ -14,9 +15,7 @@ def abs_file_paths(directory):
 
 folder_path = pathlib.Path
 
-#Calculate centroid for every sound file and average for each distance
-
-print("Averaged centroid from distance 1 to distance 5:")
+#Calculate centroid for every file
 
 #Distance 1
 sound_file_paths = [f for f in abs_file_paths("/Users/hannahsmacbook/Stimuli/Stimuli_J/dis_1")]
@@ -26,16 +25,12 @@ for sound_file_path in sound_file_paths:
     cen = sound.spectral_feature(feature='centroid')
     res_list_1.extend(cen)
 
-'''
-#To see all values separately:
-print("Values for distance 1:")
-print(res_list_1)
-#To see which values match which file:
-print(sound_file_paths)
-'''
+#Get list of all values for distance 1
+'''print(res_list_1)'''
 
-c_1=(statistics.mean(res_list_1))
-c_sd_1= (statistics.stdev(res_list_1))
+#Get mean centroid and stdev for distance 1
+c_1 = (statistics.mean(res_list_1))
+c_sd_1 = (statistics.stdev(res_list_1))
 
 
 #Distance 2
@@ -45,16 +40,13 @@ for sound_file_path in sound_file_paths:
     sound = slab.Binaural(sound_file_path)
     cen = sound.spectral_feature(feature='centroid')
     res_list_2.extend(cen)
-'''
-#To see all values separately:
-print("Values for distance 2:")
-print(res_list_2)
-#To see which values match which file:
-print(sound_file_paths)
-'''
 
-c_2=(statistics.mean(res_list_2))
-c_sd_2= (statistics.stdev(res_list_2))
+#Get list of all values for distance 2
+'''print(res_list_2)'''
+
+#Get mean centroid and stdev for distance 2
+c_2 = (statistics.mean(res_list_2))
+c_sd_2 = (statistics.stdev(res_list_2))
 
 
 #Distance 3
@@ -65,16 +57,12 @@ for sound_file_path in sound_file_paths:
     cen = sound.spectral_feature(feature='centroid')
     res_list_3.extend(cen)
 
-'''
-#To see all values separately:
-print("Values for distance 3:")
-print(res_list_3)
-#To see which values match which file:
-print(sound_file_paths)
-'''
+#Get list of all values for distance 3
+'''print(res_list_3)'''
 
-c_3=(statistics.mean(res_list_3))
-c_sd_3= (statistics.stdev(res_list_3))
+#Get mean centroid and stdev for distance 3
+c_3 = (statistics.mean(res_list_3))
+c_sd_3 = (statistics.stdev(res_list_3))
 
 
 #Distance 4
@@ -85,16 +73,12 @@ for sound_file_path in sound_file_paths:
     cen = sound.spectral_feature(feature='centroid')
     res_list_4.extend(cen)
 
-'''
-#To see all values separately:
-print("Values for distance 4:")
-print(res_list_4)
-#To see which values match which file:
-print(sound_file_paths)
-'''
+#Get list of all values for distance 4
+'''print(res_list_4)'''
 
+#Get mean centroid and stdev for distance 4
 c_4 = (statistics.mean(res_list_4))
-c_sd_4= (statistics.stdev(res_list_4))
+c_sd_4 = (statistics.stdev(res_list_4))
 
 
 #Distance 5
@@ -105,24 +89,21 @@ for sound_file_path in sound_file_paths:
     cen = sound.spectral_feature(feature='centroid')
     res_list_5.extend(cen)
 
-'''
-#To see all values separately:
-print("Values for distance 5:")
-print(res_list_5)
-#To see which values match which file:
-print(sound_file_paths)
-'''
+#Get list of all values for distance 5
+'''print(res_list_5)'''
 
+#Get mean centroid and stdev for distance 5
 c_5 = (statistics.mean(res_list_5))
-c_sd_5= (statistics.stdev(res_list_5))
+c_sd_5 = (statistics.stdev(res_list_5))
 
 
-#Final list with averaged centroid values for each distance
+#List with averaged centroid values for each distance
+print("Mean centroid from category 1 to 5:")
 final_list_mean = [c_1, c_2, c_3, c_4, c_5]
 print(final_list_mean)
 
-#Calculate standard deviation for each distance
-print("Standard deviation for each distance:")
+#List with standard deviation for each category
+print("Standard deviation from category 1 to 5:")
 final_list_stdev = [c_sd_1, c_sd_2, c_sd_3, c_sd_4, c_sd_5]
 print(final_list_stdev)
 
@@ -132,14 +113,9 @@ rel_cen = [x / c_1 for x in final_list_mean]
 print(rel_cen)
 
 
-
 #Creating boxplot
 #Overall list with all values for each distance
-overall_list= [res_list_1, res_list_2, res_list_3, res_list_4, res_list_5]
-
-'''
-print(overall_list)
-'''
+overall_list = [res_list_1, res_list_2, res_list_3, res_list_4, res_list_5]
 
 fig = plt.figure()
 fig.suptitle('Centroid over distance categories', fontsize=10)
@@ -150,8 +126,7 @@ ax.boxplot(overall_list)
 ax.set_xlabel('Distance category')
 ax.set_ylabel('Centroid')
 
-'''
 plt.show()
 '''
-plt.savefig("Centroid_pilot")
-
+plt.savefig("Centroid")
+'''
