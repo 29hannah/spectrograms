beb499da82f05204beeada1ed7de280f5858ef59
29hannah <hannah.ziesenies@gmail.com>
2022-04-27 16:26:32 +0200

    get file with sound_id and distance

--- behavioral_data.py
+++ behavioral_data.py
@@ -24,17 +24,27 @@ path_5 = list(pathlib.Path('/Users/hannahsmacbook/Pilot_stimuli_2').glob('*dis5*
 sound_file_names = [path_1, path_2, path_3, path_4, path_5]
 
 
-seq = slab.Trialsequence(conditions=[1, 2, 3, 4, 5], n_reps=10)
+seq = slab.Trialsequence(conditions=[1, 2, 3, 4, 5], n_reps=1)
 
 for condition in seq:
     file_name_group = sound_file_names[condition - 1]
     file_name = random.choice(file_name_group)
     slab.Sound.play_file(file_name)
     with slab.key() as key:  # wait for a key press
-        response = key.getch()
+        key_code = key.getch()
+    if key_code == 49:
+        response = 1
+    elif key_code == 50:
+        response = 2
+    elif key_code == 51:
+        response = 3
+    elif key_code == 52:
+        response = 4
+    elif key_code == 53:
+        response = 5
     response_obj = {
         'sound_id': file_name.stem,
-        'reponse': response
+        'response': response
     }
     seq.add_response(response_obj)
 out_file = 'participant_name'
